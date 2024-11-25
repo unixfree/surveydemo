b9152ae63b44df180f7454ac91af4a019b7afcea
unixfree <kwangrag.son@gmail.com>
2024-11-26 01:34:10 +0900

    Update 03_user.sh

--- prepare/03_user.sh
+++ prepare/03_user.sh
@@ -1,8 +1,8 @@
-curl --location -g -X POST 'http://localhost:4985/travel-sample/_user/' \
+curl --location -g -X POST 'http://localhost:4985/survey/_user/' \
 -H "Authorization: Basic ${DIGEST}" \
 -H 'Content-Type: application/json' -H 'Accept: application/json' \
--d '{ "name": "syncgateway",
-      "password": "password",
-      "admin_roles": ["stdrole"],
-      "admin_channels": ["public"] 
+-d '{ "name": "investigator",
+      "password": "Passw0rd!",
+      "admin_roles": ["survey_rw_role"],
+      "admin_channels": ["survey_ch"] 
     }'
